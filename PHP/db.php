a961befa8d10f5a8a1fa1c876ff0ea9639c859b3
lucasgardes <lucas.gardes@limayrac.fr>
2025-07-20 18:21:10 +0200

    mise en ligne

--- PHP/db.php
+++ PHP/db.php
@@ -2,7 +2,7 @@
 require_once '../../config.php';
 
 $pdo = new PDO(
-    'mysql:host=localhost;dbname=u370019086_cartes_animees;charset=utf8',
+    'mysql:host=mysql.hostinger.com;dbname=u370019086_cartes_animees;charset=utf8',
     'u370019086_root',
     BDD_MDP
 );
