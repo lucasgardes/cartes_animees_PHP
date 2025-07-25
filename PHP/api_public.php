12eba3601f73a2c14f6f1eb036edb22f3a50b702
lucasgardes <lucas.gardes@limayrac.fr>
2025-07-25 18:46:24 +0200

    Mise en ligne

--- PHP/api_public.php
+++ PHP/api_public.php
@@ -1,7 +1,5 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 require_once '../../api/api.php';
 ?>
