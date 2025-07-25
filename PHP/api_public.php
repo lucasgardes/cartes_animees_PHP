961409b7945d0fca1120fe74f3494bea3a771e1d
lucasgardes <lucas.gardes@limayrac.fr>
2025-07-25 18:22:57 +0200

    Ajout du fichier api

--- PHP/api_public.php
+++ PHP/api_public.php
@@ -1,6 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
 require_once '../../api/api.php';
 ?>
